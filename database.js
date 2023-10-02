b116c054326bd42963ad5070a526502ab81b1019
Gamer Edd <eddgtx@hotmail.com>
2023-10-02 16:15:21 -0500

    yy
    yy

--- database.js
+++ database.js
@@ -9,16 +9,17 @@ const dbConfig = {
   port: 3306, // Cambia esto al número de puerto de tu servidor de MySQL si es diferente
 };
 
-// Crear una conexión a la base de datos
-const connection = mysql.createConnection(dbConfig);
+// Crear una pool de conexiones a la base de datos
+const pool = mysql.createPool(dbConfig);
 
-// Conectar a la base de datos
-connection.connect((error) => {
+// Verificar la conexión a la base de datos
+pool.getConnection((error, connection) => {
   if (error) {
     console.error('Error al conectar a la base de datos:', error);
     throw error;
   }
   console.log('Conexión a la base de datos MySQL establecida');
+  connection.release(); // Liberar la conexión inmediatamente
 });
 
-module.exports = connection;
+module.exports = pool;
